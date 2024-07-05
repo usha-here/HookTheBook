678cae6d1e4a38ad55e3b9e30ceeb07d855880ba
usha-here <usha.20225099@mnnit.ac.in>
2024-07-05 18:55:00 +0530

    footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-black text-white px-8 py-4'>
+      <h1 className='text-xl font-semibold text-center'>
+        &copy; 2024 , Made With Love By TheAlphacon
+      </h1>
+    </div>
   )
 }
 
