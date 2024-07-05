86b933e99a2864e5b2f8c3767d9279694a60a583
usha-here <usha.20225099@mnnit.ac.in>
2024-07-05 17:19:48 +0530

    navbar and footer structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Home from './pages/Home'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </div>
   )
 }
 
