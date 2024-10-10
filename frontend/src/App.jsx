2651737590a70bd954f6c39e360e7bed257017e9
Usha Nitwal <ushanitwal09@gmail.com>
2024-10-10 23:09:55 +0530

    reduc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Home from './pages/Home'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import {Routes,Route} from "react-router-dom";
 import Aboutus from './pages/Aboutus';
 import AllBooks from './pages/AllBooks';
 import LogIn from './pages/LogIn';
@@ -14,7 +14,6 @@ import ViewBookDetails from './components/ViewBookDetails/ViewBookDetails';
 const App = () => {
   return (
     <div>
-      <Router>
       <Navbar/>
       <Routes>
         <Route exact path='/' element={<Home/>}/>
@@ -27,7 +26,6 @@ const App = () => {
         <Route path='view-book-details/:id' element={<ViewBookDetails/>}/>
       </Routes>
       <Footer/>
-      </Router>
       
       </div>
   )
